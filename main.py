f553788105444799a90015e7e658dabd89b15d5e
Antonio Mori <camargomoritrancoso@hotmail.com>
2023-10-25 21:33:41 -0300

    creating class

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+#bearbed theme ruby
+#criando modelo de dados usando pydantic basemodel
+class ProdutoCriar(BaseModel):
+    codigo: int
+    nome: str
+    quantidade: int
+    preco: float
+
+#get , post(criar) , put(atualizar) , delete
 produtos = [ 
     {
-        codigo: 1,
-        nome: "notebooks",
-        quantidade: 10,
-        preco: 2000
+        "codigo": 1,
+        "nome": "notebooks",
+        "quantidade": 10,
+        "preco": 2000
     
     }]
 
@@ -15,14 +25,33 @@ produtos = [
 def raiz():
     return "ola mundo"
 
+
 @app.get("/produtos")
 def listagem_de_produtos():
     return produtos
 
-@app.get("/produtos/{codigo}")
-def listar_produto_por_codigo(codigo):
-    return codigo
 
+@app.get("/produtos/{codigo}")
+def listar_produto_por_codigo(codigo: int):
+    for i in produtos:
+        if i["codigo"] == codigo:
+            return i
+        else:
+            return "Produto não encontrado"
+
+@app.post("/produtos")
+def cadastrar_produto(dados: ProdutoCriar):
+    produtos.append(
+        {
+            "codigo": dados.codigo,
+            "nome": dados.nome,
+            "quantidade": dados.quantidade,
+            "preco": dados.preco
+        }
+    )
+    return "Produto cadastrado com sucesso"
+
+#atividadezinha
 @app.get("/usuarios")
 def listar_usuarios_por_cpf():
     return "List de usuarios por cpf"
